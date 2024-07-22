8047801a50095f45af03bac1496e9ea976128db3
hanssonjennie <jhansson@uoguelph.ca>
2024-07-22 17:26:04 -0400

    V3 2024-07-22

--- SBT-NC-LTCdata.R
+++ SBT-NC-LTCdata.R
@@ -17,6 +17,8 @@ library(rsk)
 library(transducer)
 library(hydrorecipes)
 library(data.table)
+library(tidyverse)
+library(fs)
 
 #remotes::install_github("jkennel/rsk")
 #remotes::install_github("jkennel/transducer")
@@ -26,15 +28,18 @@ library(data.table)
 #dbar_to_m <- 1.0199773
 
 #Covert time and date to the right format.
-s <- as.POSIXct("2024-06-07", tz = "UTC")
-e <- as.POSIXct("2024-07-03", tz = "UTC")
+start <- as.POSIXct("2024-06-07", tz = "UTC")
+end <- as.POSIXct("2024-07-03", tz = "UTC")
 
 # Set file directory:
-file_dir <- "C:/Users/jhansson/OneDrive - University of Guelph/Sulfolane-hansson workfolder/02 RScripts/Transducer data/North Creek/hydrostream/data/"
+file_dir <- "data/"
 
 # Read excel file: 
-filepath <- c("C:/Users/jhansson/OneDrive - University of Guelph/Sulfolane-hansson workfolder/02 RScripts/Transducer data/North Creek/hydrostream/data/")
-loc <-readxl::read_xlsx(path=paste0(filepath,"Input_North Creek.xlsx"),
+#filepath <- c("data/")
+# loc <-readxl::read_xlsx(path=paste0(filepath,"Input_North Creek.xlsx"),
+#                         sheet = 'Sheet1',
+#                         range ="A1:J41")
+loc <-readxl::read_xlsx("data/Input_North Creek.xlsx",
                         sheet = 'Sheet1',
                         range ="A1:J41")
 
@@ -43,30 +48,47 @@ setDT(loc)
 loc <- loc[site =="SBT" & well == "UT1-FCT-008-3"]
 
 # search for matches to argument pattern within each element of a character vector:
-loc <-loc[grep("CSV", file_name)]
+loc <-loc[grep(".csv", file_name)]
 
 #Make a list of the file names in the folder (TURE= entire path, FALSE= file name only:
-#file name <- list.files(file_dir, full.names = TRUE, pattern = "*.csv")
-
-fn <- list.files("./data", full.names = TRUE, pattern = "*.csv")
+fn <- list.files(file_dir, full.names = TRUE, pattern = "*.csv")
 fn <- fn[basename(fn) %in% loc$file_name]
 
+#set file path to read in all csv files in the folder:
+file_paths <- fs::dir_ls("data/")
+
+file_contents <- list()
+
+for (i in seq_along(file_paths)) {
+  file_contents[[i]] <- read_csv(
+    file = file_paths[[i]],
+    skip = 13
+  )
+}
+
+file_contents <- set_names(file_contents, file_paths)
+file_paths %>% 
+  map(function (path) {
+    read_csv(path)
+  })
+
+dataset <- as.data.frame(file_contents)
+
 #Read the transducer data specified based on the file name, and file number in the table uploaded to R:
- 
-transducer::read_transducer(fn[21])
+##pressure <- read.csv("./Data/1094394_FCT-008-3_2024_07_04_232952.csv",
+                    # header = TRUE, skip = 13)
 
-pressure <- read.csv("1094394_FCT-008-3_2024_07_04_232952.csv",
-                     header = TRUE)
-pressure <-fread()
+#Choosing one variable to plot: 
+##Pressure 
 
-pressure <-read_csv(fn[1:6],
-                    return_data_table = FALSE,
-                    include_params = c('file_name'),
-                    keep_raw = TRUE,
-                    raw = TRUE)
+#pressure <- pressure[variable %in% c("LEVEL")]
 
+#pressure <- pressure %in% ("LEVEL")
 
-pressure <- pr[variable %in% c("LEVEL")]
-pressure[, file_name := gsub('../../hydrostream/data/', '', file_name, fixed = TRUE)]
-#### committing to test 1 
-#test 2
+#pressure[, file_name := gsub('../../hydrostream/data/', '', file_name, fixed = TRUE)]
+
+#pr <- loc[pr, on = "file_name"]
+
+baro <-[port == "baro_nc"]
+wl <- pr[!port %in% c("baro_nc")]
+wl <- ba[, list(datetime, baro = value)][wl, on = "datetime", nomatch = 0]
